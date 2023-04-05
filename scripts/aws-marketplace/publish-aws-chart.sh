42753bc82f361b8ffc414c40b5412dcb080fb3dd
Ritesh Patel <ritesh@nirmata.com>
2023-04-05 11:47:52 -0700

    Update new version with the chart fixes requested by AWS

--- scripts/aws-marketplace/publish-aws-chart.sh
+++ scripts/aws-marketplace/publish-aws-chart.sh
@@ -1,2 +1,6 @@
 export HELM_EXPERIMENTAL_OCI=1
 aws ecr get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin 709825985650.dkr.ecr.us-east-1.amazonaws.com
+
+VERSION=v1.10.0-07
+helm package aws
+helm push kyverno-chart-${VERSION}-aws.tgz oci://709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/
