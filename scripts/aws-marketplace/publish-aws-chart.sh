576a1ba7cd45eca5283aa547af725ce3a3ad5336
Parikshit Samant <parikshit@nirmata.com>
2023-04-17 10:11:30 +0530

    upgrade to v1.9.2

--- scripts/aws-marketplace/publish-aws-chart.sh
+++ scripts/aws-marketplace/publish-aws-chart.sh
@@ -1,6 +1,6 @@
 export HELM_EXPERIMENTAL_OCI=1
 aws ecr get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin 709825985650.dkr.ecr.us-east-1.amazonaws.com
 
-CHART_VERSION=v1.11.2-01-aws
+CHART_VERSION=v1.11.2-02-aws
 helm package aws
 helm push kyverno-chart-${CHART_VERSION}.tgz oci://709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/
