400508258764707be84aabdd06f81bd0de17044d
Anushka Mittal <138426011+anushkamittal2001@users.noreply.github.com>
2023-11-28 16:49:47 +0530

    Revert publish-aws-images; image change (#195)
    * Revert publish-aws-images; image change
    
    Signed-off-by: anushkamittal2001 <anushka@nirmata.com>
    
    * Chart version
    
    Signed-off-by: anushkamittal2001 <anushka@nirmata.com>
    
    * n4k version
    
    Signed-off-by: anushkamittal2001 <anushka@nirmata.com>
    
    * n4k version
    
    Signed-off-by: anushkamittal2001 <anushka@nirmata.com>
    
    * match chart versio in delivery-options file
    
    * Fix volumemount iin chart
    
    ---------
    
    Signed-off-by: anushkamittal2001 <anushka@nirmata.com>
    Co-authored-by: Parikshit Samant <parikshit@nirmata.com>

--- scripts/aws-marketplace/publish-aws-images.sh
+++ scripts/aws-marketplace/publish-aws-images.sh
@@ -1,12 +1,24 @@
-N4K_VERSION="${N4K_VERSION:-v1.10.5-n4k.nirmata.2-aws}"
+N4K_VERSION="${N4K_VERSION:-v1.10.5-n4k.nirmata.2}"
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 709825985650.dkr.ecr.us-east-1.amazonaws.com
 
-regctl image copy ghcr.io/nirmata/kyvernopre:$N4K_VERSION 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/kyvernopre:$N4K_VERSION
-regctl image copy ghcr.io/nirmata/kyverno:$N4K_VERSION 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/kyverno:$N4K_VERSION
-regctl image copy ghcr.io/nirmata/cleanup-controller:$N4K_VERSION 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/cleanup-controller:$N4K_VERSION
-regctl image copy ghcr.io/nirmata/reports-controller:$N4K_VERSION 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/reports-controller:$N4K_VERSION
-regctl image copy ghcr.io/nirmata/background-controller:$N4K_VERSION 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/background-controller:$N4K_VERSION
+docker pull --platform linux/amd64 ghcr.io/nirmata/kyverno:$N4K_VERSION
+docker pull --platform linux/amd64 ghcr.io/nirmata/kyvernopre:$N4K_VERSION
+docker pull --platform linux/amd64 ghcr.io/nirmata/cleanup-controller:$N4K_VERSION
+docker pull --platform linux/amd64 ghcr.io/nirmata/background-controller:$N4K_VERSION
+docker pull --platform linux/amd64 ghcr.io/nirmata/reports-controller:$N4K_VERSION
+
+docker tag ghcr.io/nirmata/kyverno:$N4K_VERSION 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/kyverno:$N4K_VERSION
+docker tag ghcr.io/nirmata/kyvernopre:$N4K_VERSION 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/kyvernopre:$N4K_VERSION
+docker tag ghcr.io/nirmata/cleanup-controller:$N4K_VERSION 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/cleanup-controller:$N4K_VERSION
+docker tag ghcr.io/nirmata/background-controller:$N4K_VERSION 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/background-controller:$N4K_VERSION
+docker tag ghcr.io/nirmata/reports-controller:$N4K_VERSION 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/reports-controller:$N4K_VERSION
+
+docker push 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/kyverno:$N4K_VERSION
+docker push 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/kyvernopre:$N4K_VERSION
+docker push 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/cleanup-controller:$N4K_VERSION
+docker push 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/background-controller:$N4K_VERSION
+docker push 709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/reports-controller:$N4K_VERSION
 
 aws ecr list-images --registry-id 709825985650 --repository-name nirmata/kyverno --region us-east-1 | grep -C3 $N4K_VERSION
 aws ecr list-images --registry-id 709825985650 --repository-name nirmata/kyvernopre --region us-east-1 | grep -C3 $N4K_VERSION
