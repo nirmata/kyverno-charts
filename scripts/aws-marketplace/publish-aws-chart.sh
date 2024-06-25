620a80a055da242e7666543ef0f6aeb88f374f28
Anushka Mittal <anushka@nirmata.com>
2024-06-25 18:19:24 +0530

    1.12.5
    Signed-off-by: Anushka Mittal <anushka@nirmata.com>

--- scripts/aws-marketplace/publish-aws-chart.sh
+++ scripts/aws-marketplace/publish-aws-chart.sh
@@ -1,6 +1,6 @@
 export HELM_EXPERIMENTAL_OCI=1
 aws ecr get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin 709825985650.dkr.ecr.us-east-1.amazonaws.com
 
-CHART_VERSION="${CHART_VERSION:-1.12.1}"
+CHART_VERSION="${CHART_VERSION:-1.12.5}"
 helm package aws
 helm push kyverno-chart-${CHART_VERSION}.tgz oci://709825985650.dkr.ecr.us-east-1.amazonaws.com/nirmata/
